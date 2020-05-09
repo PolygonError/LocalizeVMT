85e1314126257d886de1bff4696b5bd25c310d64
poly <18281973+PolygonError@users.noreply.github.com>
2020-05-10 02:15:24 +1000

    Update utility.cpp
    Fixed the search function never actually using the given delimiter, previously just using a hard coded character.

--- utility.cpp
+++ utility.cpp
@@ -10,7 +10,7 @@
 std::vector<std::string> Utility::split(std::string pString, std::string pDelimit) {
 	size_t pos = 0;
 	std::vector<std::string> tokens;
-	while ((pos = pString.find("/")) != std::string::npos) {
+	while ((pos = pString.find(pDelimit)) != std::string::npos) {
 		tokens.push_back(pString.substr(0, pos));
 		pString.erase(0, pos + 1);
 	}
